4a0b937b4f88183e3ae963bbdc0230102089194a
Alexandr_Lukomskiy <Lukomskiy.AC@gmail.com>
2026-03-06 16:39:06 +0200

    Jenkins settings

--- src/components/Layout.tsx
+++ src/components/Layout.tsx
@@ -4,7 +4,7 @@ export const Layout = () => {
     return (
         <>
             <header>
-                <h1>Header</h1>
+                <h1>MENU</h1>
                 <nav style={{display: 'flex', gap: '30px'}}>
                     <Link to={'/'}>Home</Link>
                     <Link to={'/about'}>About</Link>
