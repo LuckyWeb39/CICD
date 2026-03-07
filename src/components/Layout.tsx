48b558fdc6d6798eae91ace8d8b37d19cfe9f5c8
Alexandr_Lukomskiy <Lukomskiy.AC@gmail.com>
2026-03-07 11:11:36 +0200

    Jenkins settings

--- src/components/Layout.tsx
+++ src/components/Layout.tsx
@@ -7,8 +7,8 @@ export const Layout = () => {
                 <h1>Jenkins</h1>
                 <nav style={{display: 'flex', gap: '30px'}}>
                     <Link to={'/'}>Home</Link>
-                    <Link to={'/about'}>About CI/CD</Link>
-                    <Link to={'/counter'}>Counter 123</Link>
+                    <Link to={'/about'}>About ME</Link>
+                    <Link to={'/counter'}>Counter CI</Link>
                 </nav>
             </header>
             <main style={{flex: '1'}}>
